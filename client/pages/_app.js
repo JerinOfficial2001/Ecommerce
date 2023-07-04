759d0706bd284a93d1a680fc719527e74178ef7d
jerin2001 <jerinjerijree@gmail.com>
2023-07-04 15:32:51 +0530

    apicall

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,13 +2,18 @@ import "@/styles/globals.css";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import productsReducer, { productsFetch } from "../src/redux/productsSlices";
+import { productsApi } from "@/src/redux/productsApi";
 
 export default function App({ Component, pageProps }) {
   const store = configureStore({
     reducer: {
       products: productsReducer,
+      [productsApi.reducerPath]: productsApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(productsApi.middleware),
   });
+
   store.dispatch(productsFetch());
   return (
     <Provider store={store}>
